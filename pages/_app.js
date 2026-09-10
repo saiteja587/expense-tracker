297e0475054cb9d4e6c93868edcfbc7c414bb26c
saiteja587 <pulipatisaiteja12@gmail.com>
2026-09-10 17:29:59 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch(() => {});
+    }
+  }, []);
+
   return (
     <>
       <Head>
         <title>Expense Ledger</title>
         <meta name="description" content="Personal expense tracker" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#241F1A" />
+        <link rel="icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Ledger" />
       </Head>
       <Component {...pageProps} />
     </>
